c56ffc986a86943e8dce7ce74df0241fda847617
susiantiamelia <ameliasusianti@gmail.com>
2018-07-27 13:23:00 +0700

    create room box

---
+++ client/src/views/createRoom.vue
@@ -0,0 +1,12 @@
+<template>
+    <roomBox></roomBox>
+</template>
+
+<script>
+import roomBox from '@/components/createRoom.vue'
+export default {
+    components: {
+        roomBox
+    }
+}
+</script>
