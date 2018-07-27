0dbf85017fbe0efb230bba0eebc2a846e0307cfd
susiantiamelia <ameliasusianti@gmail.com>
2018-07-27 08:37:39 +0700

    first box

---
+++ client/src/components/nicknamebox.vue
@@ -0,0 +1,26 @@
+<template>
+    <div id="nickname">
+        <v-container fluid>
+            <v-form>
+                <v-layout row wrap>
+                    <v-text-field
+                        label="Regular"
+                        placeholder="Placeholder"
+                    ></v-text-field>
+                </v-layout>
+            </v-form>
+        </v-container>
+    </div>
+</template>
+
+<script>
+export default {
+    
+}
+</script>
+
+
+<style>
+
+</style>
+
--- client/src/views/About.vue
+++ client/src/views/About.vue
@@ -1,5 +1,38 @@
 <template>
   <div class="about">
-    <h1>This is an about page</h1>
+    <v-container>
+      <v-layout justify-center>
+        <v-flex xs7>
+              <v-card>
+                    <v-card-media
+                        src="https://i0.wp.com/www.remodelaholic.com/wp-content/uploads/2017/07/ginger-tic-tac-toe-board-at-GingerSnapCrafts.com_.jpg?resize=533%2C800&ssl=1"
+                        height="500px"
+                      ></v-card-media>
+
+                        <nickname></nickname>
+                      
+                      <v-card-actions>
+                        <v-btn id="nickname" color="green">Submit</v-btn>
+                      </v-card-actions>
+                  </v-card>
+            </v-flex>
+      </v-layout>
+    </v-container>  
   </div>
 </template>
+
+<script>
+import nickname from '@/components/nicknamebox.vue'
+export default {
+  components: {
+    nickname
+  }
+}
+</script>
+
+<style scoped>
+#nickname{
+  width: 100%;
+}
+</style>
+
